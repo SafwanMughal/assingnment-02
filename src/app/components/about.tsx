64db5031139e3b2ebd48230baecb7c6840ef00e4
SafwanMughal <safwanmughal.9@gmail.com>
2024-11-24 23:51:21 +0500

    second assingnment

---
+++ src/app/components/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>About This App</h2>
+      <p>This is a simple application built with Next.js to demonstrate component creation and routing.</p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header";
+import About from "./components/about";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
